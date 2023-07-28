18c7d4a11f6178a7f8d61621c7d139a4cb348f53
Your Name <you@example.com>
2023-07-29 03:21:33 +0600

    Added DB_USER & DB_PASSWORD in vercel.com

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o7ia8pd.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.o7ia8pd.mongodb.net/?retryWrites=true&w=majority`;
 
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
