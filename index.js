03aa5ffe02b29405a71e7cba217a5eb0a7dd35d7
Your Name <you@example.com>
2023-07-29 03:04:17 +0600

    Added username in Mongodb Database access

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.o7ia8pd.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o7ia8pd.mongodb.net/?retryWrites=true&w=majority`;
 
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
